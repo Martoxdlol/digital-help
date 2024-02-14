ccd9c582adacc8c4b039ccc16b13d9b475e8bb33
Tomás Cichero <martoxdlol@gmail.com>
2024-02-14 13:09:29 -0300

    agregar id a main

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
-import serviceWorker from "astrojs-service-worker";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [serviceWorker()],
+    integrations: [],
 });
